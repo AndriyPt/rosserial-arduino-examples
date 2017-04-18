cb624e15e4c5c43db7dfb4c61845d14d27e041e4
Andriy Petlovanyy <andriy@todo.todo>
2017-04-18 00:07:27 +0000

    First version of the firmware

--- first_hardware/firmware/firmware.ino
+++ first_hardware/firmware/firmware.ino
@@ -1,28 +1,33 @@
-/*
- * rosserial Publisher Example
- * Prints "hello world!"
- */
-
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+#include <first_msgs/GetMotorSpeed.h>
 
 ros::NodeHandle nh;
+float currentEffort = 0.0;
 
-std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
+void commandCallback(const std_msgs::Float32& command_message)
+{
+  currentEffort = command_message.data;
+}
 
-char hello[13] = "hello world!";
+void getMotorVelocity(const first_msgs::GetMotorSpeed::Request  &request, first_msgs::GetMotorSpeed::Response &response)
+{
+  response.velocity = 10.0 * currentEffort;
+}
+
+ros::Subscriber<std_msgs::Float32> effort_command_subscriber("send_motor_1_effort", &commandCallback);
+ros::ServiceServer<first_msgs::GetMotorSpeed::Request, first_msgs::GetMotorSpeed::Response> motor_velocity_service(
+  "get_motor_velocity", &getMotorVelocity);
 
 void setup()
 {
   nh.initNode();
-  nh.advertise(chatter);
+  nh.subscribe(effort_command_subscriber);
+  nh.advertiseService(motor_velocity_service);
 }
 
 void loop()
 {
-  str_msg.data = hello;
-  chatter.publish( &str_msg );
   nh.spinOnce();
-  delay(1000);
+  delay(10);
 }
