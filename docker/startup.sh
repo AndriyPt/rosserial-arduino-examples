a7707611b16a859cc98520827ccb01a542d97edf
Andriy Petlovanyy <Andriy@noemail.com>
2019-10-14 22:58:47 +0000

    Fixed gzweb

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,6 +5,6 @@ sudo rm -f /tmp/.X1-lock
 nohup Xvfb :1 -screen 0 1024x768x16 &> /home/user/gzweb/xvfb.log &
 
 pushd /home/user/c9sdk
-project_home=/home/user/workspace/first/base/src/first/
+project_home=/home/user/workspace/rosserial-arduino-examples/base/src/first/
 node ./server.js --listen 0.0.0.0 --port 8181 -w $project_home -a :
 popd
